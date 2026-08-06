3c1ea3762d36998836be544ef1a951507c46b46b
wallissonghost-code <wallissonghost@gmail.com>
2026-08-06 18:35:01 -0300

    Atualiza cache do PWA para painel zerado

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fut-live-v3';
-const ASSETS=['/','/index.html','/styles.css','/admin.css','/app.js','/realtime.js','/manifest.webmanifest','/icon.svg'];
+const CACHE='fut-live-v4';
+const ASSETS=['/','/index.html','/styles.css','/admin.css','/preboot.js','/app.js','/realtime.js','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
