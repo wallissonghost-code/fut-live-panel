99b9b952bfae0b5a3ae613f10e2043066f646e01
wallissonghost-code <wallissonghost@gmail.com>
2026-08-07 18:09:40 -0300

    Refresh PWA cache for single-screen battle layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fut-live-v9';
+const CACHE='fut-live-v10';
 const ASSETS=['/','/index.html','/styles.css','/admin.css','/battle.css','/preboot.js','/team-gifts.js','/battle.js','/realtime.js','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
