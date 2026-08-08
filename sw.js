00209b18bef71013fa99980b8192754dcf0e0b3b
wallissonghost-code <wallissonghost@gmail.com>
2026-08-08 15:13:07 -0300

    Bump PWA cache after battle image controls update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fut-live-v11';
+const CACHE='fut-live-v12';
 const ASSETS=['/','/index.html','/styles.css','/admin.css','/battle.css','/preboot.js','/battle.js','/realtime.js','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
