8764c1a0661327551065996b242d17a093a9fa1a
wallissonghost-code <wallissonghost@gmail.com>
2026-08-08 15:43:01 -0300

    Atualizar cache para regras de pontuação

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fut-live-v13';
+const CACHE='fut-live-v14';
 const ASSETS=['/','/index.html','/styles.css','/admin.css','/battle.css','/battle-tutorial.css','/preboot.js','/battle.js','/realtime.js','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
